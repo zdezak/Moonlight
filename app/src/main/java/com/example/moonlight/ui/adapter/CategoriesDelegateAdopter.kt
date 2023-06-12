e5e745a34d11defef9133ad82205ce73a85b5bcd
zdezak <zdezarus@gmail.com>
2023-06-12 14:52:22 +0500

    add image from json and onClick on constrainLayout

--- app/src/main/java/com/example/moonlight/ui/adapter/CategoriesDelegateAdopter.kt
+++ app/src/main/java/com/example/moonlight/ui/adapter/CategoriesDelegateAdopter.kt
@@ -1,6 +1,11 @@
 package com.example.moonlight.ui.adapter
 
+import android.graphics.drawable.Drawable
 import android.view.View
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
+import com.example.moonlight.R
 import com.example.moonlight.data.model.Category
 import com.example.moonlight.databinding.ItemCategoryBinding
 
@@ -8,10 +13,26 @@ class CategoriesDelegateAdopter(private val clickListener: (View) -> Unit) :
     ViewBindingDelegateAdapter<Category, ItemCategoryBinding>(ItemCategoryBinding::inflate) {
     override fun ItemCategoryBinding.onBind(item: Category) {
         nameCategory.text = item.name
-        nameCategory.setOnClickListener(clickListener)
-//        tvTitle.text = item.title
-//        imgBg.setOnClickListener(clickListener)
-//        imgBg.setImageResource(item.imageRes)
+        container.setOnClickListener(clickListener)
+        Glide
+            .with(container)
+            .load(item.imageUrl)
+            .centerCrop()
+            .placeholder(R.drawable.loading_spinner)
+            .into(
+                object : CustomTarget<Drawable>() {
+                    override fun onLoadCleared(placeholder: Drawable?) {
+                        container.background = placeholder
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable,
+                        transition: Transition<in Drawable>?,
+                    ) {
+                        container.background = resource
+                    }
+                }
+            )
     }
 
     override fun isForViewType(item: Any): Boolean = item is Category
