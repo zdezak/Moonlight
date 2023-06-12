23fbb0017cade9f6b9c33269bdd35a3ba8dcc7d6
zdezak <zdezarus@gmail.com>
2023-06-12 21:56:04 +0500

    add DialogFragment

--- app/src/main/java/com/example/moonlight/ui/adapter/DishesDelegateAdapter.kt
+++ app/src/main/java/com/example/moonlight/ui/adapter/DishesDelegateAdapter.kt
@@ -1,12 +1,14 @@
 package com.example.moonlight.ui.adapter
 
+import android.view.View
 import com.bumptech.glide.Glide
 import com.example.moonlight.data.model.Dish
 import com.example.moonlight.databinding.ItemDishBinding
 
-class DishesDelegateAdapter :
+class DishesDelegateAdapter(private val clickListener: (View) -> Unit) :
     ViewBindingDelegateAdapter<Dish, ItemDishBinding>(ItemDishBinding::inflate) {
     override fun ItemDishBinding.onBind(item: Dish) {
+        container.setOnClickListener(clickListener)
         dishName.text = item.name
         Glide.with(container).load(item.image_url).into(dishImage)
     }
---
+++ app/src/main/java/com/example/moonlight/ui/dish/DishFragment.kt
@@ -0,0 +1,76 @@
+package com.example.moonlight.ui.dish
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.bumptech.glide.Glide
+import com.example.moonlight.R
+import com.example.moonlight.databinding.FragmentDishBinding
+import com.example.moonlight.ui.category.CategoryUiState
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class DishFragment : DialogFragment() {
+    private val viewModel: DishViewModel by viewModels()
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        val binding = FragmentDishBinding.inflate(layoutInflater)
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.CREATED) {
+
+                viewModel.categoryUiState.collect { value ->
+                    val dishIndex = viewModel.getDishIndex()
+                    when (value) {
+                        is CategoryUiState.Loading -> {
+                            binding.name.text = getString(R.string.status_loading)
+                        }
+
+                        is CategoryUiState.Success -> {
+                            binding.name.text = value.dishes[dishIndex].name
+                            binding.description.text = value.dishes[dishIndex].description
+                            binding.cost.text = value.dishes[dishIndex].price
+                            binding.weight.text = value.dishes[dishIndex].weight
+                            Glide.with(binding.root)
+                                .load(value.dishes[dishIndex].image_url)
+                                .into(binding.image)
+                            binding.close.setOnClickListener { dismiss() }
+//                          binding.addToCart.setOnClickListener{}
+                        }
+
+                        is CategoryUiState.Error -> {
+                            binding.name.text = getString(R.string.status_error)
+                        }
+
+                    }
+                }
+            }
+        }
+        return binding.root
+    }
+
+
+    companion object {
+        const val TAG = "PurchaseConfirmationDialog"
+    }
+
+    override fun show(manager: FragmentManager, tag: String?) {
+        //viewModel.setDishIndex(tag!!)
+        super.show(manager, tag)
+    }
+
+}
